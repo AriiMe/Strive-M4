def8768c8402ece68c155ad8b044bebab1b7c24f
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-17 19:42:39 +0100

    changed navbar bit

--- Day2/homework/src/Components/NavBar.jsx
+++ Day2/homework/src/Components/NavBar.jsx
@@ -10,14 +10,19 @@ import { Navbar, Nav, Form, Button, FormControl, NavDropdown } from "react-boots
 const NavBar = (props) => {
     
   return (
-    <div>
-      <Navbar id="navbaru" bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Yeetttttt</Navbar.Brand>
+    <div id="nav">
+      <Navbar bg="black" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="https://i.pinimg.com/originals/9b/d8/8c/9bd88c8e74d666fed60487f95f419c12.png"
+        width="60"
+        height="50"
+        className="d-inline-block align-top"
+      />{' '}
+      AAAAAAAAAAA
+    </Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-
     <NavDropdown title="Genres" id="collasible-nav-dropdown">
         <NavDropdown.Item href="" value="Fantasy">Fantasy</NavDropdown.Item>
         <NavDropdown.Divider />
@@ -33,10 +38,12 @@ const NavBar = (props) => {
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
     </Form>
-    
   </Navbar>
     </div>
   );
 };
+
+
+
 
 export default NavBar;
