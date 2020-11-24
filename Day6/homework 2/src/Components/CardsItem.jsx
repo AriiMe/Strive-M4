11dfdb6a75b150f9921471024b017391d0d4ac6b
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-24 18:01:36 +0100

    fixed card size

--- Day6/homework 2/src/Components/CardsItem.jsx
+++ Day6/homework 2/src/Components/CardsItem.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Image } from 'react-bootstrap';
-import { propTypes } from 'react-bootstrap/esm/Image';
-import './card.css'
+/** @format */
 
+import React from "react";
+import PropTypes from "prop-types";
+import { Image } from "react-bootstrap";
+import { propTypes } from "react-bootstrap/esm/Image";
+import "./card.css";
 
 class CardsItem extends React.Component {
+  render() {
+    const { movie, onClick } = this.props;
 
-    render() {
+    return (
+      <div
+        class="col-sm-12 col-md-6 col-lg-2 mt-3 mb-5 mr-5 text-center"
+        onClick={onClick}
+      >
+        <Image src={movie.Poster} className=".img-fluid " fluid />
 
-        const { movie, onClick } = this.props;
-        
-        return (
-
-            <div class="col-sm-12 col-md-6 col-lg-2 mx-auto text-center" onClick={onClick}>
-                <Image src={movie.Poster} className=".img-fluid" fluid />
-                <div class="details text-left col-sm-12">
-                    <p class="title m-0">{movie.Title}</p>
-                    <p class="text-muted m-0">{movie.Type}</p>
-                    <p class="text-truncate m-0 ">{movie.Year}</p>
-                </div>
-            </div>
-
-        )
-    }
+        <div class="details text-left col-sm-12">
+          <p class="title m-0">{movie.Title}</p>
+          <p class="text-muted m-0">{movie.Type}</p>
+          <p class="text-truncate m-0 ">{movie.Year}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 CardsItem.propTypes = { movie: propTypes.Object };
-
-
 
 export default CardsItem;
