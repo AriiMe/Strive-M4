7ea7e34cd57dae515d527db597bd04421642a89e
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-26 15:14:13 +0100

    day 9

---
+++ Day9/src/Components/SignUp.jsx
@@ -0,0 +1,199 @@
+/** @format */
+
+import React, { Component } from "react";
+import { Button, Col, Row, Container } from "react-bootstrap";
+
+export class SignUp extends Component {
+  render() {
+    return (
+      <>
+        <h3 className="whatever">Sign up not for free</h3>
+
+        <Container>
+          <h6 className="text-center font-weight-bold mt-4 mb-4">
+            Sign up with your email address
+          </h6>
+          <form id="form">
+            <div className="form-group">
+              <label for="email">What is your email address? </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email your email address."
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label for="email2">Confirm email address </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email2"
+                placeholder="Enter your email address again"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label for="password">Create a password </label>
+              <input
+                type="password"
+                className="form-control mb-3"
+                placeholder="Create a password."
+                id="password"
+                required
+              />
+            </div>
+            <div className="form-group mb-4">
+              <label for="nickname">GIVE ME YOUR CREDIT CARD NOW </label>
+              <input
+                type="text"
+                className="form-control mb-2"
+                id="nicname"
+                placeholder="giv cash big boy."
+                required
+              />
+            </div>
+            <label className="form-group" for="bdate">
+              What's your date of birth?
+            </label>
+            <div className="form-row mb-4" id="bdate">
+              <Col md={5}>
+                <label for="day">Month</label>
+                <select id="inputState" className="form-control" required>
+                  <option className="text-muted">Month</option>
+                  <option>January</option>
+                  <option>February</option>
+                  <option>March</option>
+                  <option>April</option>
+                  <option>May</option>
+                  <option>June</option>
+                  <option>July</option>
+                  <option>August</option>
+                  <option>September</option>
+                  <option>October</option>
+                  <option>November</option>
+                  <option>December</option>
+                </select>
+              </Col>
+              <Col md={3}>
+                <label for="day">Day</label>
+                <input
+                  type="number"
+                  id="day"
+                  className="form-control"
+                  placeholder="TT"
+                  required
+                />
+              </Col>
+              <Col md={2}>
+                <label for="year">year</label>
+                <input
+                  type="number"
+                  id="year"
+                  className="form-control"
+                  placeholder="YYYY"
+                  required
+                />
+              </Col>
+            </div>
+            <label className="form-group" for="gender">
+              What's your gender?
+            </label>
+            <div
+              className="form-group d-flex justify-content-between col-10"
+              id="gender"
+            >
+              <div className="form-check" id="gender">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="Male"
+                  value="option1"
+                />
+                <label className="form-check-label" for="Male">
+                  {" "}
+                  Male{" "}
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="Female"
+                  value="option2"
+                />
+                <label className="form-check-label" for="Female">
+                  {" "}
+                  Female{" "}
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="Divers"
+                  value="option3"
+                />
+                <label className="form-check-label" for="Divers">
+                  {" "}
+                  Non-binary{" "}
+                </label>
+              </div>
+            </div>
+
+            <label
+              className="form-check-label checkbox font-weight-light mt-3 mb-3 d-flex justify-content-center"
+              for="marketing"
+            >
+              <input type="checkbox" id="marketing" />
+              <span className="checkmark"></span>
+              Share my registration data with aris phishing site content
+              providers for marketing purposes.
+            </label>
+
+            <label
+              className="form-check-label checkbox font-weight-light mt-0 mb-5"
+              for="terms"
+            >
+              <span className="checkmark byClick"></span>
+              By clicking on Sign up, you agree to Terms and Conditions of Use.
+              <a href="#" target="_blank" className="red">
+                Terms of Use
+              </a>
+              .
+              <p className="mt-0">
+                To learn more about how Ari collects, uses, shares and sells to
+                china your personal data please read no dont read
+                <a href="#" target="_blank" className="red">
+                  Privacy Policy
+                </a>
+                .
+              </p>
+            </label>
+
+            <button
+              type="submit"
+              className="btn btn-danger col-12 col-6 mb-4 signUpBtn"
+              id="signUp"
+            >
+              Sign Me Up Fam
+            </button>
+            <p className="text-center">
+              You already have an account?
+              <a href="#" className="red">
+                Sign in
+              </a>{" "}
+              .
+            </p>
+          </form>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default SignUp;
---
+++ Day9/src/Components/jumbo.jsx
@@ -0,0 +1,63 @@
+/** @format */
+
+import React from "react";
+import { Jumbotron, Container, Image } from "react-bootstrap";
+import "../App.css";
+import Jumbovid from "../assets/Jumbovid.mp4";
+import ReactPlayer from "react-player/youtube";
+
+class Jumbo extends React.Component {
+  render() {
+    return (
+      <Jumbotron
+        fluid
+        className="p-0 jumbotron"
+        id="jumbotron"
+        style={{ width: "100%", height: "100vh", position: "relative" }}
+      >
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{
+            position: "relative",
+
+            width: "100%",
+            height: "120vh",
+            zIndex: "0",
+            overflow: "hidden",
+          }}
+        >
+          <video
+            src={Jumbovid}
+            autoPlay
+            muted
+            loop
+            style={{
+              position: "relative",
+              width: "100%",
+              marginBottom: "25vh",
+              objectFit: "cover",
+            }}
+          ></video>
+        </div>
+
+        <div
+          className="gradientOnJumbo"
+          style={{
+            content: "",
+            background: "rgb(20, 20, 20)",
+            background:
+              "linear-gradient(180deg, rgba(20, 20, 20, 0) 34%, rgba(20, 20, 20, 1) 100%)",
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0",
+            zIndex: "3",
+          }}
+        ></div>
+      </Jumbotron>
+    );
+  }
+}
+
+export default Jumbo;
